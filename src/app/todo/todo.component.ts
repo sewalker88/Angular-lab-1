f24d208677eb7486a4d67f97c7b0f15c9bcfd284
Shawna Walker <sewalker88@yahoo.com>
2020-08-18 12:01:37 -0400

    commit for Angular lab2

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Todo } from '../interfaces/todo';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
@@ -20,8 +21,45 @@ export class TodoComponent implements OnInit {
       completed: false,
     },
   ];
-
+  searchTerm: string;
+  showIndex: number;
   constructor() {}
+
+  setShowIndex(index: number) {
+    this.showIndex = index;
+  }
+
+  resetShowIndex() {
+    this.showIndex = undefined;
+  }
+  addTask(form: NgForm) {
+    let newTodo: Todo = {
+      task: form.value.task,
+      completed: false,
+    };
+    this.todos.push(newTodo);
+  }
+  removeTask(index: number) {
+    this.todos.splice(index, 1);
+  }
+
+  completeTask(index: number) {
+    this.todos[index].completed = true;
+  }
+
+  setSearchTerm(form: NgForm) {
+    this.searchTerm = form.value.searchTerm.toLowerCase().trim();
+  }
+  filter() {
+    if (!this.searchTerm) {
+      return this.todos;
+    } else {
+      return this.todos.filter((chore) => {
+        let currentTodo = chore.task.toLowerCase().trim();
+        return currentTodo.includes(this.searchTerm);
+      });
+    }
+  }
 
   ngOnInit(): void {}
 }
